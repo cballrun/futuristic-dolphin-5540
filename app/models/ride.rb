4135ebeb9419778a2cdd895cd007d9ba4505edb5
Carter Ball <carter.k.ball@gmail.com>
2022-09-12 11:41:31 -0400

    Create MechanicRides, set up model relationships for Mechanic, Ride, MechanicRides

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,4 +1,6 @@
 class Ride < ApplicationRecord
+  has_many :mechanic_rides
+  has_many :mechanics, through: :mechanic_rides
   belongs_to :amusement_park
   validates_presence_of :name, :thrill_rating, :open
 end
