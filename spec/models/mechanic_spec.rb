0c6ac3b04717f0672761162dc9975d10f797dab0
Carter Ball <carter.k.ball@gmail.com>
2022-09-12 11:31:41 -0400

    Test updates

--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -7,7 +7,8 @@ RSpec.describe Mechanic, type: :model do
   end
 
   describe 'relationships' do
-    it { should have_many :rides}
+    it { should have_many :mechanic_rides }
+    it { should have_many(:rides).through(:mechanic_rides) }
   end
 
 end
